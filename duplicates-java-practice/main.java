01dd5f0a74d16cd3afab0b31372a8c3ccc926fe3
chadiboudaher <chadiboudaher7@gmail.com>
2025-07-26 19:56:48 +0300

    latest changes

--- duplicates-java-practice/main.java
+++ duplicates-java-practice/main.java
@@ -1,4 +1,21 @@
-class main {
-public static void main(String[] args) {
-    
-}}
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 3, 5, 3, 2};
+        System.out.println(firstDuplicates(nums)); 
+
+    }
+
+    public static int firstDuplicates(int[] nums) {
+        HashSet hash = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.contains(nums[i])) {
+                return nums[i];
+            }
+            hash.add(nums[i]);
+        }
+        return -1;
+    }
+}
