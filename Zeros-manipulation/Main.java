60d13bf0ed996216a4afa6aa93dc305277eb4492
chadiboudaher <chadiboudaher7@gmail.com>
2025-07-31 12:06:16 +0300

    New Methods

--- Zeros-manipulation/Main.java
+++ Zeros-manipulation/Main.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 0, 2, 0, 3};
-        zerosToFront(nums);
+        // int[] nums = {1, 0, 2, 0, 3};
+        // zerosToFront(nums);
+        int[] nums = {1, -2, 3, -1, -3, -4, 5};
+        System.out.println(Arrays.toString(negativeToEnd(nums)));
 
     }
     public static void zerosToFront(int[] nums) {
@@ -19,5 +21,19 @@ public class Main {
             nums[prefix--] = 0;
         }
         System.out.println(Arrays.toString(nums));
+    }
+
+    public static int[] negativeToEnd(int[] nums) {
+        int prefix = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                int temp = nums[i];
+                nums[i] = nums[prefix];
+                nums[prefix] = temp;
+                prefix++;
+            }
+        }
+        return nums;
     }
 }
